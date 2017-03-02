3ed0ca76164bdc9e7a6518918fd0e9bb9bc451dc
adamwoodland <adamwoodland@icloud.com>
2017-03-02 14:57:39 -0400

    Changed the methods so it returns an int and not a boolean

--- src/main/java/com/example/email_validator/email_validator/Validator.java
+++ src/main/java/com/example/email_validator/email_validator/Validator.java
@@ -1,30 +1,15 @@
 package com.example.email_validator.email_validator;
 
 public class Validator {
-	
-	private String email;
-	
-	public Validator (String e) {
-		email = e;
-	}
-	
-	public String getEmail ( ) {
-		return email;
-	}
-	
-	public void setEmail (String e) {
-		email = e;
-	}
 
-	public boolean validate ( ) {
-		if (email.matches("[^@]*@[^@]*") && email.matches(".*[.].*"))
-			return true;
-		else
-			return false;
+	//checks to see if a String follow two formatting rules
+	public int validate (String email) {
+		int x = 0;
+		if (email.matches("[^@]*@[^@]*"))
+			x++;
+		if ( email.matches(".*[.].*"))
+			x++;
+		return x;
 	}
 	
-	public static void main (String [] args) {
-		Validator validator = new Validator("adamwoodland@icloud.com");
-		System.out.print(validator.validate());
-	}
 }
