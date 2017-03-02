29e9de6a9f2dc1b55e8e1449e927d5e35de0d81c
adamwoodland <adamwoodland@icloud.com>
2017-03-02 15:01:12 -0400

    Changed the method to account for four different formatting rules

--- src/main/java/com/example/email_validator/email_validator/Validator.java
+++ src/main/java/com/example/email_validator/email_validator/Validator.java
@@ -1,12 +1,20 @@
 package com.example.email_validator.email_validator;
 
 public class Validator {
-
-	public boolean validate ( String email) {
-		if (email.matches("[^@]*@[^@]*") && email.matches(".*[.].*") && email.matches(".*[A-Za-z0-9]-*[A-Za-z0-9].*") && email.matches(".*[.]+((com)|(ca))"))
-			return true;
-		else
-			return false;
+	
+	//checks to see if a String follows four formatting rules
+	public int validate (String email) {
+		int x = 0;
+		if (email.matches("[^@]*@[^@]*"))
+			x++;
+		if (email.matches(".*[.].*"))
+			x++;
+		if (email.matches(".*[A-Za-z0-9]-*[A-Za-z0-9].*"))
+			x++;
+		if (email.matches(".*[.]+((com)|(ca))"))
+			x++;
+		return x;
 	}
-
+			
 }
+
